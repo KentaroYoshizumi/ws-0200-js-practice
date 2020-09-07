7ffe55221d2f7b14d13425bf9c4ede02b0281eee
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2020-09-07 20:01:17 +0900

    Update 001_base.js

--- 001_syntax/001_base.js
+++ 001_syntax/001_base.js
@@ -4,75 +4,108 @@
  */
 function helloWorld() {
 }
+console.log("Hello World");
 
 /**
  *  1.2 const を使って"hoge"という文字列を定義した変数を定義し、それを出力する関数を定義してください
  *
  */
 function displayConst() {
+  const a = "hoge";
+  return a;
 }
+  console.log(displayConst());
+
+
 
 /**
  *  1.3 let を使って"hoge"という文字列を定義した変数を定義し、それを出力する関数を定義してください
  *
  */
 function displayLet() {
-}
+    let a = "hoge";
+    return a;
+  }
+    console.log(displayLet());
+
 
 /**
  *  1.4 引数に渡した変数を表示するメソッドを実装してください
  *
  */
 function displayArgument(arg) {
+  console.log(arg);
 }
+displayArgument('arg');
 
 /**
  *  1.5 渡された二つの引数の合計を返却するメソッドを実装してください
  *
  */
 function sumTwoArgs(a, b) {
+  return a + b;
 }
+  console.log(sumTwoArgs(2, 5));
 
 /**
  *  1.6 渡された二つの引数の差を返却するメソッドを実装してください
  *
  */
 function subtractTwoArgs(a, b) {
+  return a - b;
 }
+  console.log(subtractTwoArgs(5, 2));
 
 /**
  *  1.7 渡された二つの引数の商を返却するメソッドを実装してください。0での割り算はnullを返却してください。
  *
  */
 function quotientTwoArgs(a, b) {
+  return a / b;
 }
+  console.log(quotientTwoArgs(4, 2));
+
 
 /**
  *  1.8 渡された二つの引数の積を返却するメソッドを実装してください
  *
  */
 function productTwoArgs(a, b) {
+  return a * b;
 }
+  console.log(productTwoArgs(4, 2));
+
 
 /**
  *  1.9 渡された引数の2で割ったあまりを返却するメソッドを実装してください
  *
  */
 function remainderTwoArgs(a) {
+  return 5 % 2;
 }
+  console.log(remainderTwoArgs());
 
 /**
  *  1.10 渡された引数が2の倍数であることを確認するメソッドを実装してください
  *
  */
 function isEven(a) {
+  var a = 9;
+
+if (a % 2 == 0){
+  console.log("偶数です");
+} else {
+	console.log("奇数です");
 }
+
 
 /**
  *  1.11 渡された二つの文字列を連結するメソッドを実装してください
  *
  */
 function concatString(a, b) {
+const str = "a" + "b";
+console.log(str);
 }
 
 module.exports = {
